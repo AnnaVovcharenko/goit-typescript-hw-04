ffa77bea0a3ab4e25cb736fe38d178f4d6472cd4
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-12-01 15:29:25 +0200

    1

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -4,20 +4,31 @@ import noop from "lodash/noop";
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
-// Додати тип Menu Selected
+type SelectedMenu = {
+  id: MenuIds
+};
 
+type MenuSelected = {
+  selectedMenu: SelectedMenu
+};
+
+
+// Додати тип Menu Selected
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: {id: "first"},
 });
 
 // Додайте тип MenuAction
+type MenuAction = {
+  onSelectedMenu: (argo: SelectedMenu) => void;
+};
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +59,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
